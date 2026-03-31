3b894c3fd78767584e97b323b2375fc6aa741864
kyoung-mo <kym1290306@gmail.com>
2026-03-31 12:45:51 +0900

    2804 fin

--- Implementation/2804_크로스워드만들기.c
+++ Implementation/2804_크로스워드만들기.c
@@ -1,31 +1,30 @@
+// https://www.acmicpc.net/problem/2804
 #include <stdio.h>
 #include <string.h>
 
 int main(){
     int row=-1, col=-1, rsize, csize, chk=0;
-    char a[30], b[30];
+    char a[100]={0}, b[100]={0};
     scanf("%s %s",a,b);
 
-    // printf("sizeof(a)=%d, strlen(a)=%d",sizeof(a), strlen(a));
+    if(strlen(a) > 30 || strlen(b) > 30) return -1;
 
     rsize = strlen(b);
     csize = strlen(a);
-   
-    
-    for(int i=0;i<rsize;i++){
-        for(int j=0;j<csize;j++){
+
+    for(int i=0;i<csize;i++){ 
+        for(int j=0;j<rsize;j++){ 
             if(a[i]==b[j]) 
             {
-                col=j; 
-                row=i;
-                //printf("col = %d, row = %d",col, row);
-                chk=1;
+                row=i; 
+                col=j;
+                i=csize;
                 break;
             }
-            else continue;
         }
         if(chk==1) break;
     }
+    
     if(row==-1||col==-1) return -1;
 
     for(int i=0;i<rsize;i++){
