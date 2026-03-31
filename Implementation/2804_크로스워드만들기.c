66f01c21c68e52284a329030678f479b8422dde8
kyoung-mo <kym11290306@gmail.com>
2026-03-31 09:31:21 +0900

    update 2804

--- Implementation/2804_크로스워드만들기.c
+++ Implementation/2804_크로스워드만들기.c
@@ -2,25 +2,15 @@
 #include <string.h>
 
 int main(){
-    int row, col, rsize, csize, chk=0;
-    char a[]="REPUBLIKA", b[]="HRVATSKA";
+    int row=-1, col=-1, rsize, csize, chk=0;
+    char a[30], b[30];
+    scanf("%s %s",a,b);
 
     // printf("sizeof(a)=%d, strlen(a)=%d",sizeof(a), strlen(a));
 
     rsize = strlen(b);
     csize = strlen(a);
-    
-    for(int i=0;i<rsize;i++){
-        for(int j=0;j<csize;j++){
-            if(a[i]==b[j]) 
-            {
-                col=i; 
-                row=j;
-                break;
-            }
-            else continue;
-        }
-    }
+   
     
     for(int i=0;i<rsize;i++){
         for(int j=0;j<csize;j++){
@@ -28,7 +18,7 @@ int main(){
             {
                 col=j; 
                 row=i;
-                printf("col = %d, row = %d",col, row);
+                //printf("col = %d, row = %d",col, row);
                 chk=1;
                 break;
             }
@@ -36,14 +26,13 @@ int main(){
         }
         if(chk==1) break;
     }
-    
-    printf("\n\n");
+    if(row==-1||col==-1) return -1;
 
     for(int i=0;i<rsize;i++){
         for(int j=0;j<csize;j++){
-            if(j==row) printf("%c ",b[i]);
-            else if(i==col) printf("%c ", a[j]);
-            else printf(". ");
+            if(j==row) printf("%c",b[i]);
+            else if(i==col) printf("%c", a[j]);
+            else printf(".");
         }
         printf("\n");
     }
