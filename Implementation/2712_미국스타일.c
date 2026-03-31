a7f5f3f0e99070b37b5814d87a67120bcaa8422b
kyoung-mo <kym1290306@gmail.com>
2026-04-01 08:39:40 +0900

    add 0331_2712_미국스타일

--- Implementation/2712_미국스타일.c
+++ Implementation/2712_미국스타일.c
@@ -1,33 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    double amount;
-    char status[10]={0};
+    double amount=0;
+    char status[3]="0";
 
-    scanf("%lf %s",&amount,status);
+    //printf("%lf %s",amount, status);
 
-    if(*status=="kg"){
-        amount*=2.2046;
-        *status="lb";
-        printf("%.4lf %s\n",amount, status);
+    while(1){
+        scanf("%lf %s",&amount,status);
+        if(amount==-1) break;
+        if(!strcmp(status,"kg")){
+            amount*=2.2046;
+            strcpy(status,"lb");
+            printf("%.4lf %s",amount, status);
+            chk=-1;
+        }
+        else if(!strcmp(status,"lb")){
+            amount*=0.4536;
+            strcpy(status,"kg");
+            printf("%.4lf %s",amount, status);
+            amount=-1;
+        }
+        else if(!strcmp(status,"l")){
+            amount*=0.2642;
+            strcpy(status,"g");
+            printf("%.4lf %s",amount, status);
+            amount=-1;
+        }
+        else if(!strcmp(status,"g")){
+            amount*=3.7854;
+            strcpy(status,"l");
+            printf("%.4lf %s",amount, status);
+            amount=-1;
+        }
+        else return 0;
     }
-     if(*status=="l"){
-        amount*=0.2642;
-        *status="g";
-        printf("%.4lf %s\n",amount, status);
-    }
-    else if(*status=="lb"){
-        amount*=0.4536;
-        *status="kg";
-        printf("%.4lf %s\n",amount, status);
-    }
-    else if(*status=="g"){
-        amount*=3.7854;
-        *status="l";
-        printf("%.4lf %s\n",amount, status);
-    }
-    else return -1;
-
     return 0;
 }
