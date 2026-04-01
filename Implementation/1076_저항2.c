32c3cd2859eacf3e7d89d670f333c131a5d6a94c
kyoung-mo <kym11290306@gmail.com>
2026-04-02 02:11:44 +0900

    add 1076

---
+++ Implementation/1076_저항2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+void prt_zero(int rep, int result);
+
+int main(){
+    char color[10][10]={"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
+    char top[10],mid[10],bottom[10];
+    int result=0;
+
+    scanf("%s\n%s\n%s",top,mid,bottom);
+    
+    for(int i=0;i<10;i++)
+    {
+        if(!strcmp(color[i],top)) result+=i;
+    }
+    result*=10;
+    for(int i=0;i<10;i++)
+    {
+        if(!strcmp(color[i],mid)) result+=i;
+    }
+    printf("%d",result);
+    for(int i=0;i<10;i++)
+    {
+        if(!strcmp(color[i],bottom)) prt_zero(i,result);
+    }
+    
+    
+    return 0;
+}
+
+void prt_zero(int rep, int result){
+    int square_result=1;
+    if(result==0){}
+    else if(rep==0){}
+    else
+    {
+        for(int i=0;i<rep;i++)printf("0");
+    }
+}
