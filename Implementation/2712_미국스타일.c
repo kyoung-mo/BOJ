8d1cfa812a970801e7f63a73a92403ce8f62efc3
kyoung-mo <kym1290306@gmail.com>
2026-04-01 20:07:51 +0900

    complete 2712

--- Implementation/2712_미국스타일.c
+++ Implementation/2712_미국스타일.c
@@ -1,3 +1,4 @@
+// https://www.acmicpc.net/problem/2712
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
@@ -5,34 +6,36 @@
 int main(){
     double amount=0;
     char status[3]="0";
-
+    int cnt=-1;
+    scanf("%d",&cnt);
+    if(cnt==-1) return -1;
     //printf("%lf %s",amount, status);
 
-    while(1){
+    for(int i=0;i<cnt;i++){
         scanf("%lf %s",&amount,status);
         if(amount==-1) break;
         if(!strcmp(status,"kg")){
             amount*=2.2046;
             strcpy(status,"lb");
-            printf("%.4lf %s",amount, status);
-            chk=-1;
+            printf("%.4lf %s\n",amount, status);
+            amount=-1;
         }
         else if(!strcmp(status,"lb")){
             amount*=0.4536;
             strcpy(status,"kg");
-            printf("%.4lf %s",amount, status);
+            printf("%.4lf %s\n",amount, status);
             amount=-1;
         }
         else if(!strcmp(status,"l")){
             amount*=0.2642;
             strcpy(status,"g");
-            printf("%.4lf %s",amount, status);
+            printf("%.4lf %s\n",amount, status);
             amount=-1;
         }
         else if(!strcmp(status,"g")){
             amount*=3.7854;
             strcpy(status,"l");
-            printf("%.4lf %s",amount, status);
+            printf("%.4lf %s\n",amount, status);
             amount=-1;
         }
         else return 0;
