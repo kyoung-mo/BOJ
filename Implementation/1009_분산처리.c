950155c6fc2abc178bf77b4151b8042f8bc96c94
kyoung-mo <kym1290306@gmail.com>
2026-04-05 20:46:57 +0900

    add 1009

--- Implementation/1009_분산처리.c
+++ Implementation/1009_분산처리.c
@@ -2,13 +2,13 @@
 
 int main(){
     int cnt=0;
-    int a=0,b=0,last_num=1;
+    
 
     scanf("%d",&cnt);
 
     for(int i=0;i<cnt;i++){
+        int a=0,b=0,last_num=1;
         scanf("%d %d",&a,&b);
-
         for(int i=0;i<b;i++){
             last_num*=a;
             last_num=last_num%10;
@@ -20,7 +20,6 @@ int main(){
             
         }
         printf("%d\n",last_num);
-        last_num=1;
     }
     return 0;
 }
